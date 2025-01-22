12a632f974768d7b2ad0bfe20cf6c16d119aca12
Björn Lindqvist <bjourne@gmail.com>
2025-01-22 12:32:57 +0100

    Better variable names

--- tests/test_torch.py
+++ tests/test_torch.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2024 Björn A. Lindqvist
+# Copyright (C) 2024-2025 Björn A. Lindqvist
 from collections import namedtuple
 from einops import rearrange
 from myopencl.objs import MyContext
-from myopencl.utils import (can_compile, is_gpu, platform_device_pairs)
+from myopencl.utils import can_compile, is_gpu, platform_device_pairs
 from pathlib import Path
 from pytest import mark
 from torch.nn.functional import conv2d, pad
@@ -85,17 +85,17 @@ def test_conv2d(platform_id, device_id):
     c.register_queue("main", [])
 
     for d in CONV2D_SETUPS:
-        filters = torch.randn(d.nout, d.nin, d.ksize, d.ksize)
-        x = torch.randn(1, d.nin, d.ih, d.iw)
+        W = torch.randn(d.nout, d.nin, d.ksize, d.ksize)
+        X = torch.randn(1, d.nin, d.ih, d.iw)
 
         # Run on torch
-        y_torch = conv2d(x, filters, padding = d.padding)
+        Y_torch = conv2d(X, W, padding = d.padding)
 
         # Run on OpenCL
-        y_cl = torch.empty_like(y_torch)
-        write_torch_tensor(c, "main", "filters", filters)
-        write_torch_tensor(c, "main", "x", x)
-        c.register_output_buffer("y", y_cl.nbytes)
+        Y_cl = torch.empty_like(Y_torch)
+        write_torch_tensor(c, "main", "filters", W)
+        write_torch_tensor(c, "main", "x", X)
+        c.register_output_buffer("y", Y_cl.nbytes)
 
         args = [
             (cl.cl_uint, d.nout),
@@ -105,15 +105,15 @@ def test_conv2d(platform_id, device_id):
             (cl.cl_uint, d.padding), "y"
         ]
         ev = c.run_kernel("main", "conv2d", "conv2d", [1], None, args)
-        ev = read_torch_tensor(c, "main", "y", y_cl)
+        ev = read_torch_tensor(c, "main", "y", Y_cl)
         cl.wait_for_events([ev])
 
-        diff = torch.abs(torch.sum(y_cl - y_torch))
+        diff = torch.abs(torch.sum(Y_cl - Y_torch))
         if diff > d.tol:
             print("== GOT ==")
-            print(y_cl)
+            print(Y_cl)
             print("== EXPECTED ==")
-            print(y_torch)
+            print(Y_torch)
         assert diff < d.tol
         c.release_all_buffers()
     c.finish_and_release()
@@ -135,28 +135,28 @@ def test_conv2d_2(platform_id, device_id):
         sy, sx = d.ih, d.iw
         fy, fx = d.ksize, d.ksize
 
-        filters = torch.randn(dc, sc, fy, fx)
-        x = torch.randn(1, sc, sy, sx)
+        W = torch.randn(dc, sc, fy, fx)
+        X = torch.randn(1, sc, sy, sx)
 
         # Run on torch
-        y_torch = conv2d(x, filters, padding = d.padding)
+        Y_torch = conv2d(X, W, padding = d.padding)
 
         # Run on OpenCL
-        n, dc, dy, dx = y_torch.shape
-        y_cl = torch.empty(n, dy, dx, dc)
+        n, dc, dy, dx = Y_torch.shape
+        Y_cl = torch.empty(n, dy, dx, dc)
 
         # Rearrange into efficient format and pad
-        x = rearrange(x, "n sc sy sx -> n sy sx sc").contiguous()
-        filters = rearrange(filters, "dc sc fy fx -> dc fy fx sc").contiguous()
+        X = rearrange(X, "n sc sy sx -> n sy sx sc").contiguous()
+        W = rearrange(W, "dc sc fy fx -> dc fy fx sc").contiguous()
 
         rem = sc % WIDTH
         n_pad = WIDTH - rem if rem else 0
-        x = pad(x, (0, n_pad), "constant", 0)
-        filters = pad(filters, (0, n_pad), "constant", 0)
+        X = pad(X, (0, n_pad), "constant", 0)
+        W = pad(W, (0, n_pad), "constant", 0)
 
-        write_torch_tensor(c, "main", "filters", filters)
-        write_torch_tensor(c, "main", "x", x)
-        c.register_output_buffer("y", y_cl.nbytes)
+        write_torch_tensor(c, "main", "filters", W)
+        write_torch_tensor(c, "main", "x", X)
+        c.register_output_buffer("y", Y_cl.nbytes)
 
         args = [
             (cl.cl_uint, dc), (cl.cl_uint, sc + n_pad),
@@ -165,13 +165,13 @@ def test_conv2d_2(platform_id, device_id):
             (cl.cl_uint, d.padding), "y"
         ]
         c.run_kernel("main", "conv2d", "conv2d", [1], None, args)
-        ev = read_torch_tensor(c, "main", "y", y_cl)
+        ev = read_torch_tensor(c, "main", "y", Y_cl)
         cl.wait_for_events([ev])
 
-        y_cl = rearrange(y_cl, "n dy dx dc -> n dc dy dx")
-        assert y_cl.shape == y_torch.shape
+        Y_cl = rearrange(Y_cl, "n dy dx dc -> n dc dy dx")
+        assert Y_cl.shape == Y_torch.shape
 
-        diff = torch.abs(torch.sum(y_cl - y_torch))
+        diff = torch.abs(torch.sum(Y_cl - Y_torch))
         if diff > d.tol:
             print("== GOT ==")
             print(y_cl)
